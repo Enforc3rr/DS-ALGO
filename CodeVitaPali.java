18bba750bcf84ea13d347f01acde6f20e5cf1e98
Prateek Kumar <59215581+Enforc3rr@users.noreply.github.com>
2020-08-15 21:19:24 +0530

    Update CodeVitaPali.java

--- CodeVitaPali.java
+++ CodeVitaPali.java
@@ -7,13 +7,19 @@ public class CodeVitaPali {
         int n2 = sc.nextInt();
         String s = "";
         int c = 0 ;
-        for(int n = n1 ; n <= n2 ;  n++){
-            for(int hh = 0 ; hh < 24 ; hh++){
-                for(int mm = 0 ; mm < 60 ; mm++){
-                    for(int sec = 0 ; sec < 60 ;sec++){
-                        s = "" + n + hh + mm + sec;
+        for(int n=n1;n<=n2; n++) {
+  			for(int h1=0;h1<=2;h1++){
+    			for(int h2=0;h2<=3;h2++) {	
+        			for(int m1=0;m1<=5;m1++) {
+            			for(int m2=0;m2<=9;m2++) {
+                			for(int s1=0;s1<=5;s1++) {
+                    			for(int s2=0;s2<=9;s2++) {
+                        s = "" + n + h1 + h2 + m1 + m2 + s1 + s2;
                         c = c + paliCheck(s);
                     }
+                }
+            }
+        }
                 }
             }
         }
