572d102b3a3dd39d86d6a7c4a980104897865139
Prateek Kumar <59215581+Enforc3rr@users.noreply.github.com>
2020-08-17 11:09:46 +0530

    Add files via upload

---
+++ KnownAlgo/SearchingUsingRecurison/BinarySearching.java
@@ -0,0 +1,20 @@
+package SearchingUsingRecurison;
+
+public class BinarySearching {
+    public static void main(String[] args) {
+
+    }
+    public static int recursiveBinarySearching(int [] A , int p , int r , int x){
+       int q = (p+r)/2;
+       if(p>r){
+           return -1;
+       }else {
+           if (A[q]==x){
+               return q;
+           }else if(A[q]>x){
+               return recursiveBinarySearching(A,p,q-1,x);
+           }else
+               return recursiveBinarySearching(A,q+1,r,x);
+       }
+    }
+}
---
+++ KnownAlgo/SearchingUsingRecurison/LinearSearching.java
@@ -0,0 +1,19 @@
+package SearchingUsingRecurison;
+
+public class LinearSearching {
+    public static void main(String[] args) {
+
+    }
+    public static int recursiveLinearSearching(int [] A , int x , int i){
+        if(i > A.length){
+            return -1;
+        }else {
+            if(A[i]==x){
+                return i;
+            }else {
+                return recursiveLinearSearching(A,x,i+1);
+            }
+        }
+
+    }
+}
